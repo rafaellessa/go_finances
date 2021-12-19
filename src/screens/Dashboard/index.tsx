b83e34cb173e64364dd30a00b8d573613ac90bad
Rafael Lessa <rafael.lessa150@gmail.com>
2021-12-19 19:33:56 -0300

    feat: create Dashboard component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Header from "../../components/Header";
+import HighlightCard from "../../components/HighlightCard";
 
-import { Container } from "./styles";
+import { Container, HeaderWrapper } from "./styles";
 
 const Dashboard: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <HeaderWrapper>
+        <Header />
+      </HeaderWrapper>
+      <HighlightCard />
+    </Container>
+  );
 };
 
 export default Dashboard;
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,3 +1,17 @@
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { RFPercentage } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const HeaderWrapper = styled.View`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.primary};
+  padding-top: ${getStatusBarHeight() + 24}px;
+  padding-left: 24px;
+  padding-right: 24px;
+  height: ${RFPercentage(42)}px;
+`;
