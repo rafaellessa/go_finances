9824e2c3f2ebf391c535e68adf79707635bbafb7
Rafael Lessa <rafael.lessa150@gmail.com>
2021-12-19 19:32:40 -0300

    chore: config and install modules

--- App.tsx
+++ App.tsx
@@ -1,8 +1,33 @@
 import React from "react";
-import { View } from "react-native";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/global/styles/theme";
+import Dashboard from "./src/screens/Dashboard";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
 
 const App: React.FC = () => {
-  return <View />;
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+      <StatusBar style="light" translucent />
+    </ThemeProvider>
+  );
 };
 
 export default App;
